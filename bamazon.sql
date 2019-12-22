0ead407bcd88d28e93774f7b18bc251202ab5681
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2019-12-21 22:08:08 -0600

    capitalized some info

--- bamazon.sql
+++ bamazon.sql
@@ -12,7 +12,7 @@ CREATE TABLE products(
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ('Topo Chico', 'Groceries', 1.17 , 500),
-		('ear phones', 'Electronics', 6.25, 627),
+		('Ear Phones', 'Electronics', 6.25, 627),
 		('Trash Bags', 'Grocery', 5.99, 300),
 		('Brawny Paper Towels', 'Grocery', 4.25, 400),
 		('Candy Apples', 'Produce', 0.35, 800),
@@ -24,5 +24,3 @@ VALUES ('Topo Chico', 'Groceries', 1.17 , 500),
         ('Baby Wipes','Children', 5.99, 750);
         
         
-        
-    
