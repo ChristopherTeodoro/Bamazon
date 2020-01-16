fb52063c5a1bea454fd1f325fb41f5296ba23e85
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-01-16 13:55:44 -0600

    cleaned up a bit

--- bamazon.sql
+++ bamazon.sql
@@ -22,14 +22,14 @@ SELECT * FROM products;
 
 /* Insertions for products table are below */
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Topo Chico', 'Groceries', 1.17 , 500),
-		('Ear Phones', 'Electronics', 6.25, 627),
-		('Trash Bags', 'Grocery', 5.99, 300),
-		('Brawny Paper Towels', 'Grocery', 4.25, 400),
-		('Candy Apples', 'Produce', 0.35, 800),
-		('Chocolate Bannana', 'Produce', 1.20, 1000),
-		('Tropicana Orange Juice', 'Grocery', 4.45, 267),
-		('Horizon Organic Milk', 'Grocery', 4.50, 200),
-		('Huggies Diapers', 'Children', 2.75, 476),
-		('Charmin Toiler Paper', 'Grocery', 12.99, 575),
-    ('Baby Wipes','Children', 5.99, 750);
+VALUES('Topo Chico', 'Groceries', 1.17, 500),
+	('Ear Phones', 'Electronics', 6.25, 627),
+	('Trash Bags', 'Grocery', 5.99, 300),
+	('Brawny Paper Towels', 'Grocery', 4.25, 400),
+	('Candy Apples', 'Produce', 0.35, 800),
+	('Chocolate Bannana', 'Produce', 1.20, 1000),
+	('Tropicana Orange Juice', 'Grocery', 4.45, 267),
+	('Horizon Organic Milk', 'Grocery', 4.50, 200),
+	('Huggies Diapers', 'Children', 2.75, 476),
+	('Charmin Toiler Paper', 'Grocery', 12.99, 575),
+	('Baby Wipes', 'Children', 5.99, 750);
