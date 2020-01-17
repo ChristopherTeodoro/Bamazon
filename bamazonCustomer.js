93db708f28ca3068decfd57aa7616fe511fd51f6
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-01-17 14:36:21 -0600

    Displaying BAMAZON products is working!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+/* creates variables that require and pull in mysql & inquirer packages */
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+/* establishes my connection to the database */
 var connection = mysql.createConnection({
   host:"localhost",
   port:3306,
@@ -9,14 +11,31 @@ var connection = mysql.createConnection({
   database:"bamazon"
 });
 
+/* console logs whether my connection worked */
 connection.connect(function(err){
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
 });
 
-// This is where the function for bamazon will go
-
-// Inquirer will go here too
+// This is where the "READ" function for displaying bamazon db is
+function readProducts() {
+  console.log("Displaying all BAMAZON products below...\n");
+  connection.query("SELECT * FROM bamazon.products;", function(err,res) {
+    if (err) throw err;
+    console.log(res);
+  });
+} 
+// This is where the "CREATE" function is
+/*function createProduct() {
+  console.log("Inserting a new product into the database...\n");
+  var query = connection.query("INSERT INTO products SET ?", {
+    children: "Similac",  
+    price: 6.25,
+    quantity: 5
+    },
+  );
+}  
 
 // prompt
 
@@ -26,4 +45,5 @@ connection.connect(function(err){
 
 // last variable here
 
-// else statement here
+// else statement here 
+*/
