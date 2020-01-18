279205762a042d08434c7826edcd1f75664b3110
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-01-17 19:14:13 -0600

    updating code to make table better looking

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,46 +4,26 @@ var inquirer = require("inquirer");
 
 /* establishes my connection to the database */
 var connection = mysql.createConnection({
-  host:"localhost",
-  port:3306,
-  user:"root",
-  password:"charlie",
-  database:"bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "charlie",
+  database: "bamazon"
 });
 
 /* console logs whether my connection worked */
-connection.connect(function(err){
+connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  readProducts();
-});
+  console.log("\n" + "Connection Succesfull!!" + "\n" + "Displaying available BAMAZON products below...\n");
+  makeTable();
+})
 
-// This is where the "READ" function for displaying bamazon db is
-function readProducts() {
-  console.log("Displaying all BAMAZON products below...\n");
-  connection.query("SELECT * FROM bamazon.products;", function(err,res) {
-    if (err) throw err;
-    console.log(res);
-  });
-} 
-// This is where the "CREATE" function is
-/*function createProduct() {
-  console.log("Inserting a new product into the database...\n");
-  var query = connection.query("INSERT INTO products SET ?", {
-    children: "Similac",  
-    price: 6.25,
-    quantity: 5
-    },
-  );
-}  
-
-// prompt
-
-// function for answer here
-
-//  update products here
-
-// last variable here
-
-// else statement here 
-*/
+// This is where the "READ" function is * Where we make the Table to display the products
+var makeTable = function () {
+  connection.query("SELECT * FROM products", function (err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " || " + res[i].product_name + " || " +
+         res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity + "\n");
+    }
+  })
+}
