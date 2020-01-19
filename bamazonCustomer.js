c2cca71af03c0d1983d9eb071054bc4e449392ce
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-01-18 18:26:46 -0600

    Final working bamazonCustomer.js!!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,7 +23,59 @@ var makeTable = function () {
   connection.query("SELECT * FROM products", function (err, res) {
     for (var i = 0; i < res.length; i++) {
       console.log(res[i].item_id + " || " + res[i].product_name + " || " +
-         res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity + "\n");
+        res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity + "\n");
+    }
+    promptCustomer(res);
+  })
+}
+// This is where Inquirer asks customer the questions
+var promptCustomer = function (res) {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'choice',
+    message: "What product would you like to purchase? [Quit with Q]"
+// This is where the code tells you what to do with the answer plus the quit =>    
+  }]).then(function (answer) {
+    var correct = false;
+    if(answer.choice.toUpperCase()=="Q"){
+      process.exit();
+    }
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].product_name == answer.choice) {
+        correct = true;
+        var product = answer.choice;
+        var id = i;
+// This is another inquirer prompt popping off
+        inquirer.prompt({
+          type: "input",
+          name: "quant",
+          message: "How many would you like to buy?",
+// Some validation checks below
+          validate: function (value) {
+            if (isNaN(value) == false) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+// Where my UPDATE occurs to my database          
+        }).then(function (answer) {
+          if ((res[id].stock_quantity - answer.quant) > 0) {
+            connection.query("UPDATE products SET stock_quantity='" + (res[id].stock_quantity - answer.quant) + "' WHERE product_name='" + product + "'", function (err, res2) {
+              console.log("Product Bought!");
+              makeTable();
+            })
+// Where a non-answer goes to
+          } else {
+            console.log("Not a valid selection!");
+            promptCustomer(res);
+          }
+        })
+      }
+    }
+    if (i == res.length && correct == false) {
+      console.log("Not a valid selection yo!");
+      promptCustomer(res);
     }
   })
 }
